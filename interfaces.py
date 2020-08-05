1a2fbf57f0db4ceef98a03026786510f943525ff
tiago.paranhos <tiagoprn@gmail.com>
2020-08-05 08:43:21 -0300

    Adding a formal interface example.

---
+++ interfaces.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+# reference: https://rednafi.github.io/digressions/python/2020/07/03/python-mixins.html
+
+
+class ICalc(ABC):
+    """Formal interface: Abstract calculator class."""
+
+    @abstractmethod
+    def add(self, a, b):
+        pass
+
+    @abstractmethod
+    def sub(self, a, b):
+        pass
+
+    @abstractmethod
+    def mul(self, a, b):
+        pass
+
+    @abstractmethod
+    def div(self, a, b):
+        pass
